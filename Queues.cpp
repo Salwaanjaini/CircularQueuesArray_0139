4fdac6de1b6f57c13155b896bc092be284be3cc5
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-12 15:49:17 +0700

    jika elemen di hapus berada di pososo terakhir array

--- Queues.cpp
+++ Queues.cpp
@@ -63,7 +63,13 @@ void remove()
         REAR = -1;
     }
     else
-   
+    {
+        // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array 
+        if (FRONT == max - 1)
+            FRONT = 0;
+        else
+            FRONT = FRONT + 1;
+    }
 
 }
 
